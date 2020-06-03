8658da21f6418269969f8a9249f396356177294b
miyoZzz <anac_tech@163.com>
2020-06-03 20:53:20 +0800

    request interface add

---
+++ backend/app/Controller/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use App\Controller;
+use Hyperf\HttpServer\Contract;
+
+class AuthController
+{
+    public function register(Contract\RequestInterface $request): string
+    {
+
+    }
+}
